299d254501938d0d8d5ef6cae70e0de64c1ee877
Volker Christian <me@vchrist.at>
2025-02-25 13:59:25 +0100

    Update doc
    Signed-off-by: Volker Christian <me@vchrist.at>

--- html/Storage_8cpp.js
+++ html/Storage_8cpp.js
@@ -1,5 +1,5 @@
 var Storage_8cpp =
 [
     [ "functions", "Storage_8cpp.html#a33a89adcfe80866f20ab94d04284b163", null ],
-    [ "voidFunctions", "Storage_8cpp.html#ac20ef2d643869d8b6859c4ce4dc2747d", null ]
+    [ "voidFunctions", "Storage_8cpp.html#a466e105feee3640432740482e6d91d87", null ]
 ];
