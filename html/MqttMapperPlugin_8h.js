55e1402ea2a0fd0bc70573f5c5f2a845848474c3
Volker Christian <me@vchrist.at>
2025-03-09 01:33:47 +0100

    Update doc
    Signed-off-by: Volker Christian <me@vchrist.at>

--- html/MqttMapperPlugin_8h.js
+++ html/MqttMapperPlugin_8h.js
@@ -5,6 +5,6 @@ var MqttMapperPlugin_8h =
     [ "mqtt::lib::VoidFunction", "structmqtt_1_1lib_1_1VoidFunction.html", "structmqtt_1_1lib_1_1VoidFunction" ],
     [ "Arguments", "MqttMapperPlugin_8h.html#a012d663296619810e0a7a2d1f0986357", null ],
     [ "json", "MqttMapperPlugin_8h.html#a2a3499b2cd7bb5321ef0aa5cc38768bc", null ],
-    [ "functions", "MqttMapperPlugin_8h.html#a33a89adcfe80866f20ab94d04284b163", null ],
-    [ "voidFunctions", "MqttMapperPlugin_8h.html#a466e105feee3640432740482e6d91d87", null ]
+    [ "functions", "MqttMapperPlugin_8h.html#a122fcc8e2540857def7d1a76450d3c00", null ],
+    [ "voidFunctions", "MqttMapperPlugin_8h.html#ac20ef2d643869d8b6859c4ce4dc2747d", null ]
 ];
